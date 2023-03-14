4a1587afe4b1986b2a5ad7be6a62c1d75356ab83
Gökhan Yıldırım <gokhan.y811@gmail.com>
2023-03-14 23:56:03 +0300

    day2

--- Week-2/day2.py
+++ Week-2/day2.py
@@ -35,3 +35,124 @@ print(metin)
 # listeler
 # döngüler
 # fonksiyonlar 
+
+dizi = ["İhtiyaç Kredisi", 10, 5.2, True]
+print(dizi)
+
+krediler = ["İhtiyaç Kredisi", "Taşıt Kredisi", "Konut Kredisi"]
+print(type(krediler))
+
+print(krediler[0])
+
+print(len(krediler))   #length
+# print(krediler[3]) =>  hata verir out of range
+
+krediler.append("Özel Kredi")
+print(krediler)
+
+krediler.append("X Kredisi")
+print(krediler)
+
+krediler.pop(0)
+print(krediler)
+
+krediler.append("Taşıt Kredisi")
+print(krediler)
+
+
+krediler.remove("Taşıt Kredisi")
+print(krediler)
+
+krediler.extend(["Y Kredisi", "Z Kredisi"])
+print(krediler)
+
+# for döngüsü
+# i=0 i<10
+
+for i in range(10):
+    print("xxx")
+    print(i)
+print("*************************")
+
+for i in range(5,11):
+    print(i)
+
+print("****************************")
+for i in range(0,51,10):
+    print(i)
+print("******************************")
+# for i in range(0.1,0,5):    hata verir. range fonksiyonu int değer kabul eder.
+#     print(i)
+krediler = ["İhtiyaç Kredisi", "Taşıt Kredisi", "Konut Kredisi"]
+for kredi in krediler:
+    print(kredi)
+print("********************")
+for i in range(len(krediler)):
+    print(krediler[i])
+print("*************************")
+
+
+# sonsuz döngü i yi arttırmazsak
+i = 0
+while i < 10:
+    print("x")
+    i += 1
+print("y")
+
+
+print("**************")
+while True:
+    print("X")
+    break
+
+print("********************")
+
+i = 0
+while i < len(krediler):
+    print(krediler[i])
+    i += 1  # Bu satırın nerede olduğuna göre çıktı değişir.
+    if krediler[i] == "Konut Kredisi":
+        break
+
+# Fonksiyonlar
+
+fiyat = 100
+indirim = 20
+# definition - fonksiyon tanımlama
+def calculate():
+    print(100-20)
+
+def calculateWithParams(fiyat,indirim):
+    print(fiyat-indirim)
+
+def sayHello(name):
+    print(f"Merhaba {name}")
+
+
+calculate()
+calculateWithParams(50,10)
+calculateWithParams(100,30)
+sayHello("Halit")
+sayHello("Arif")
+sayHello("Mevlüt")
+
+
+def calculateAndReturn(price, discount):
+    return price-discount
+
+yeniFiyat = calculateAndReturn(200,50)
+print(yeniFiyat+10)
+
+
+def calculatePrice(price, discount):
+    print(price-discount)
+
+def calculatePriceAndReturn(price, discount):
+    return price - discount
+
+print("***********************************")
+fonk1 = calculatePrice(100,50)
+fonk2 = calculatePriceAndReturn(300,100)
+print(f"159. satır : {fonk1}")
+print(f"160. satır : {fonk2+50}")
+print("*****************************")
